cde91f4c7f57898fbb0fd4956766151f64d1a8de
rohitnakashe <52915840+rohitnakashe@users.noreply.github.com>
2025-02-13 14:03:32 -0800

    Create dff.v

---
+++ dff.v
@@ -0,0 +1,14 @@
+module dff (dff_if vif);
+  
+  always@(posedge vif.clk)
+    begin
+      if(vif.rst == 1'b1)
+        vif.dout <= 1'b0;
+      else if (vif.din >= 1'b0)
+         vif.dout <= vif.din;
+      else
+         vif.dout <= 1'b0;
+    end
+  
+endmodule
+ 
